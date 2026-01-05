8a8eff4fdc51260e85f657eb615916f8f0a3a7c6
wangqing176176-a11y <wangqing176176@gmail.com>
2026-01-05 22:59:25 +0800

    Update Breadcrumb.tsx

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -7,7 +7,7 @@ const HomeCrumb = () => {
   return (
     <Link href="/" className="flex items-center">
       <FontAwesomeIcon className="h-3 w-3" icon={['far', 'flag']} />
-      <span className="ml-2 font-medium">{'Home'}</span>
+      <span className="ml-2 font-medium">{'首页'}</span>
     </Link>
   )
 }
