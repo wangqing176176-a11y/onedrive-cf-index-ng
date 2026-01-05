9d3cba6797cf415e14be450a9fcc25362920d7d4
wangqing176176-a11y <wangqing176176@gmail.com>
2026-01-05 23:13:04 +0800

    Update FolderListLayout.tsx

--- src/components/FolderListLayout.tsx
+++ src/components/FolderListLayout.tsx
@@ -53,13 +53,13 @@ const FolderListLayout = ({
   return (
     <div className="rounded bg-white shadow-sm dark:bg-gray-900 dark:text-gray-100">
       <div className="grid grid-cols-12 items-center space-x-2 border-b border-gray-900/10 px-3 dark:border-gray-500/30">
-        <div className="col-span-12 py-2 text-center text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:col-span-6">
+        <div className="col-span-12 py-2 text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:col-span-6">
           {'文件名'}
         </div>
-        <div className="col-span-3 hidden text-center text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
+        <div className="col-span-3 hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
           {'上传时间'}
         </div>
-        <div className="hidden text-center text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
+        <div className="hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
           {'大小'}
         </div>
         <div className="hidden text-center text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
