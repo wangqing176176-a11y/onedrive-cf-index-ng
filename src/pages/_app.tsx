1b540ac507751fbf70b468232ad58a2276585d7b
WangQing <wangqing176176@gmail.com>
2026-01-09 23:41:56 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -124,6 +124,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress height={1} color="rgb(156, 163, 175, 0.9)" options={{ showSpinner: false }} />
+      <AccessGate />
       <Component {...pageProps} />
     </>
   )
